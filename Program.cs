851135fd25dd760b847bec491c6d10a03285f7ff
徐州牧 <marbondxu@gmail.com>
2026-06-27 23:25:06 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int target = rand.Next(1, 101);
+        int guess = 0;
+        int count = 0;
+
+        Console.WriteLine("猜数字游戏开始！请输入 1~100 之间的数字：");
+
+        while (guess != target)
+        {
+            if (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("请输入有效数字！");
+                continue;
+            }
+
+            count++;
+            if (guess > target)
+                Console.WriteLine("太大了，再试试！");
+            else if (guess < target)
+                Console.WriteLine("太小了，再试试！");
+            else
+                Console.WriteLine($"恭喜你，猜中了！共用了 {count} 次。");
+        }
+    }
+}
